2f237586d17aa59247d0909e888447fc74520b07
Valentio <valentiocoding@gmail.com>
2025-07-20 17:51:46 +0700

    Update PWA
    Update PWA

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,8 @@ import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 import {viteStaticCopy} from "vite-plugin-static-copy"
+import { VitePWA } from "vite-plugin-pwa"
+
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -21,9 +23,33 @@ export default defineConfig({
           dest:"."
         }
       ]
+    }),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'POS Bakmie',
+        short_name: 'BakmiePOS',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        display: 'standalone',
+        start_url: '/',
+        icons: [
+          {
+            src: '/logo.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/logo.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
     })
     // require('tailwind-scrollbar-hide')
   ],
+  
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
