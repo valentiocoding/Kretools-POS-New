123fc0296c3efd57300e5cc9011e8064f9c49be5
Valentio <valentiocoding@gmail.com>
2025-07-20 16:27:24 +0700

    update redirects

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,20 @@ import path from "path"
 import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
+import {viteStaticCopy} from "vite-plugin-static-copy"
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(),
+  plugins: [react(), tailwindcss()
+    ,
+    viteStaticCopy({
+      targets:[
+        {
+          src: "public/_redirects",
+          dest:"."
+        }
+      ]
+    })
     // require('tailwind-scrollbar-hide')
   ],
   resolve: {
