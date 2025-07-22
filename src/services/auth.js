b8b70224b823350b71a62d63754852cf3269fe7a
Valentio <valentiocoding@gmail.com>
2025-07-22 16:26:27 +0700

    Update auth.js

--- src/services/auth.js
+++ src/services/auth.js
@@ -19,32 +19,3 @@ export async function handleLogout(navigate) {
 }
 
 
-
-export async function callHelloWorld() {
-  try {
-    const { data, error } = await supabase.auth.getSession();
-    if (error || !data.session) {
-      console.error("Not logged in or session error", error);
-      return;
-    }
-
-    const accessToken = data.session.access_token;
-
-    const res = await fetch("https://norhnvdhmkjjeqmpovlh.functions.supabase.co/hello-world", {
-      method: "GET",
-      headers: {
-        Authorization: `Bearer ${accessToken}`,
-      },
-    });
-
-    if (!res.ok) {
-      const err = await res.text();
-      throw new Error(`Function failed: ${res.status} - ${err}`);
-    }
-
-    const text = await res.text();
-    console.log("Function response:", text);
-  } catch (err) {
-    console.error("Fetch failed:", err.message);
-  }
-}
