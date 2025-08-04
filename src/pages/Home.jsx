52873fe5576daffdf8e548db66d629313320c8ee
Valentio <valentiocoding@gmail.com>
2025-08-04 11:56:27 +0700

    Update Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,11 @@ import React from 'react';
 import Navbar from '../components/Navbar';
 import BounceCards from '../components/BounceCards';
 import logo from '../assets/logo.png';
+import home1 from "../assets/home1.jpg"
+import home2 from "../assets/home2.jpg"
+import home3 from "../assets/home3.jpg"
+import home4 from "../assets/home4.jpg"
+import home5 from "../assets/home5.jpg"
 import { useNavigate } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 
@@ -22,7 +27,7 @@ const Home = () => {
   const navigate = useNavigate()
 
 
-  const images = [logo, logo, logo, logo, logo];
+  const images = [home1, home2,home3,home4,home5];
   const transformStyles = [
     "rotate(5deg) translate(-150px)",
     "rotate(0deg) translate(-70px)",
